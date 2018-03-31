43a1ef7cc82bee042f9fea6bee8cbdfc3f529320
caian <caian@jd.com>
2018-03-31 22:46:21 +0800

    初始话项目

---
+++ src/main/java/com/rising/sun/SunApplication.java
@@ -0,0 +1,12 @@
+package com.rising.sun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunApplication.class, args);
+	}
+}
